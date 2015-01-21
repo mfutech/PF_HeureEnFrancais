4112b27c42b9da647ab0d8d137185f88685954af
mfurrer <mfurrer@gmail.com>
2015-01-21 02:07:13 +0100

    add tap detection and switch to date screen

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <pebble.h>
 #include <french_number.h>
   
+#define COOLVETICAFONT 1
+#define KENYANCOFFEEFONT 0
+  
 static Window *s_main_window;
 static TextLayer *s_line1_layer;
 static TextLayer *s_line2_layer;
@@ -12,6 +15,14 @@ static GFont s_medium_font;
 static GFont s_small_font;
 static GFont s_tiny_font;
 
+#define LINE_LEN 15
+static char s_line1[LINE_LEN];
+static char s_line2[LINE_LEN];
+static char s_line3[LINE_LEN];
+static char s_line4[LINE_LEN];
+
+static bool date_mode = 0;
+
 void my_text_layer_set_text(TextLayer *t_layer, char *str) {
   if (strlen(str) > 8)
     text_layer_set_font(t_layer, s_small_font);
@@ -20,6 +31,20 @@ void my_text_layer_set_text(TextLayer *t_layer, char *str) {
   else
     text_layer_set_font(t_layer, s_medium_font);
   text_layer_set_text(t_layer, str);
+}
+
+static void show_date() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  text_layer_set_text(s_line1_layer, "date");
+  strftime(s_line2, LINE_LEN, "%d.%m.%Y", tick_time);
+  my_text_layer_set_text(s_line2_layer, s_line2);
+  strftime(s_line3, LINE_LEN, "heure", tick_time);
+  my_text_layer_set_text(s_line3_layer, s_line3);
+  strftime(s_line4, LINE_LEN, "%H:%M", tick_time);
+  my_text_layer_set_text(s_line4_layer, s_line4);
+
 }
 
 static void update_time() {
@@ -89,15 +114,26 @@ static void update_time() {
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
-
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+  date_mode = ! date_mode;
+  if (date_mode) show_date();
+  else update_time();
+}
 // ------------------------------------------- UI SETUP ----------------------------------------------
 static void main_window_load(Window *window) {
   // Load fonts
+#if COOLVETICAFONT
   s_big_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COOLVETICA_48));
   s_medium_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COOLVETICA_34));
   s_small_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COOLVETICA_28));
-  s_tiny_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COOLVETICA_28));
-  
+  s_tiny_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_COOLVETICA_22));
+#endif
+#if KENYANCOFFEEFONT
+  s_big_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KENYAN_COFFEE_48));
+  s_medium_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KENYAN_COFFEE_34));
+  s_small_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KENYAN_COFFEE_28));
+  s_tiny_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KENYAN_COFFEE_24));
+#endif
   // Create lines TextLayer
   int y = 2;
   s_line1_layer = text_layer_create(GRect(2, y, 140, 53));
@@ -139,12 +175,6 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_line2_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_line3_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_line4_layer));
-  
-  // Make sure the time is displayed from the start
-  update_time();
-
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 
@@ -174,7 +204,14 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-  
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // Register for tap events
+  accel_tap_service_subscribe(tap_handler);
 }
 
 static void deinit() {
