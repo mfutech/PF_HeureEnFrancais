07209f633fabe6ffa6a94867622596a49f52784d
mfutech <mfutech@users.noreply.github.com>
2016-10-29 12:31:24 +0200

    first start a clay configuration

---
+++ src/js/config.js
@@ -0,0 +1,49 @@
+module.exports = [ {
+	"type" : "heading",
+	"defaultValue" : "Heure en Français"
+}, {
+	"type" : "text",
+	"defaultValue" : "Configuration des différentes options."
+}, {
+	"type" : "section",
+	"items" : [
+	/*
+	 * { "type": "heading", "defaultValue": "More Settings" },
+	 */
+	{
+		"type" : "toggle",
+		"messageKey" : "RoudedTime",
+		"label" : "Format Heure Arrondi",
+		"defaultValue" : false
+	}, {
+		"type" : "toggle",
+		"messageKey" : "AutoReturnToTime",
+		"label" : "Retour à l'heure automatique",
+		"defaultValue" : true
+	}, {
+		"type" : "radiogroup",
+		"messageKey" : "MinuteMode",
+		"label" : "Mode des minutes",
+		"options" : [ {
+			"label" : "0-30",
+			"value" : "30"
+		}, {
+			"label" : "0-60",
+			"value" : "60"
+		} ]
+	}, {
+		"type" : "radiogroup",
+		"messageKey" : "BackgroudColor",
+		"label" : "Couleur de fond",
+		"options" : [ {
+			"label" : "Blanc",
+			"value" : "0"
+		}, {
+			"label" : "Noir",
+			"value" : "1"
+		} ]
+	} ]
+}, {
+	"type" : "submit",
+	"defaultValue" : "Save Settings"
+} ];
